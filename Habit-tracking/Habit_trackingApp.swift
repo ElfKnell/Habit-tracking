1607bf869badabaa79fbc0216ae0df04f62e4ced
Andrii Kyrychenko <akyrych84@gmail.com>
2022-08-11 14:12:58 -0300

    Initial Commit

---
+++ Habit-tracking/Habit_trackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Habit_trackingApp.swift
+//  Habit-tracking
+//
+//  Created by Andrii Kyrychenko on 11/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct Habit_trackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
